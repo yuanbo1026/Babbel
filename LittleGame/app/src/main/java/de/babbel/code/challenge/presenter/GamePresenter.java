614c20fcdefe0c63dda3bc405d421c433a7620e0
yuanbo1026 <yuanbo1026@hotmail.com>
2017-03-26 14:46:57 +0200

    create animation add start game and clear animation method create set content method

--- LittleGame/app/src/main/java/de/babbel/code/challenge/presenter/GamePresenter.java
+++ LittleGame/app/src/main/java/de/babbel/code/challenge/presenter/GamePresenter.java
@@ -1,10 +1,20 @@
 package de.babbel.code.challenge.presenter;
 
+import android.support.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import de.babbel.code.challenge.domain.DataRepository;
+import de.babbel.code.challenge.model.Word;
 import de.babbel.code.challenge.view.activity.GameActivity;
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by bo.yuan on 2017/3/26
@@ -14,9 +24,80 @@ public class GamePresenter {
     private GameActivity gameActivity;
     private DataRepository dataRepository;
 
+    private List<Word> wordList = new ArrayList<>();
+    private Word currentLanguageOneWord = new Word();
+    private Word currentLanguageTwoWord = new Word();
+    private int right_scout=0;
+    private int wrong_scout=0;
+
     @Inject
     public GamePresenter(GameActivity gameActivity, DataRepository dataRepository) {
         this.gameActivity = gameActivity;
         this.dataRepository = dataRepository;
+    }
+
+    public void getWordList() {
+        dataRepository.getWordList()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<List<Word>>() {
+                    @Override
+                    public void onCompleted() {
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        showErrorMessage(e.toString());
+                    }
+
+                    @Override
+                    public void onNext(List<Word> words) {
+                        wordList = words;
+                    }
+                });
+    }
+
+    public void initGameData() {
+    }
+
+    private void setLanguage_one_text(String text) {
+        gameActivity.setLanguage_one_text(text);
+    }
+
+    private void setLanguage_two_text(String text) {
+        gameActivity.setLanguage_two_text(text);
+    }
+
+    private Word getSingleWord(@NonNull List<Word> words) {
+        int i = new Random().nextInt(words.size());
+        return words.get(i);
+    }
+
+    public void rightButtonClicked() {
+        if (selectedRightTranslation()) {
+            gameActivity.clearAnimation();
+            setupLanguageOneWordTextView();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            gameActivity.startGame();
+            //scout +1
+        }
+    }
+
+    private void setupLanguageOneWordTextView() {
+    }
+
+    public void setupLanguageTwoWordTextView() {
+    }
+
+    private boolean selectedRightTranslation() {
+        return currentLanguageOneWord.equals(currentLanguageTwoWord);
+    }
+
+    public void showErrorMessage(String text){
+        this.gameActivity.showErrorMessage(text);
     }
 }
--- LittleGame/app/src/main/java/de/babbel/code/challenge/view/activity/GameActivity.java
+++ LittleGame/app/src/main/java/de/babbel/code/challenge/view/activity/GameActivity.java
@@ -5,13 +5,17 @@ import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.animation.Animation;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
 import de.babbel.code.challenge.R;
 import de.babbel.code.challenge.di.component.DaggerGameComponent;
@@ -27,6 +31,10 @@ public class GameActivity extends AppCompatActivity {
     TextView right;
     @BindView(R.id.wrong)
     TextView wrong;
+    @BindView(R.id.right_answer_textview)
+    TextView right_answer_textview;
+    @BindView(R.id.wrong_answer_textview)
+    TextView wrong_answer_textview;
 
     @Inject
     GamePresenter presenter;
@@ -37,6 +45,7 @@ public class GameActivity extends AppCompatActivity {
     private ObjectAnimator objectAnimator;
     private boolean isGameRunning = false;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +53,15 @@ public class GameActivity extends AppCompatActivity {
         unbinder = ButterKnife.bind(this);
         DaggerGameComponent.builder().gameModule(new GameModule(this)).build().inject(this);
         initAnimation();
+        startAnimation();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        mScreenHeight = displaymetrics.heightPixels;
     }
 
     private void initAnimation() {
@@ -67,11 +85,48 @@ public class GameActivity extends AppCompatActivity {
 
             @Override
             public void onAnimationRepeat(Animator animation) {
+                Log.d("Animation repeat","Animation repeat");
             }
         });
         objectAnimator.setDuration(DEFAULT_ANIMATION_DURATION);
         objectAnimator.setRepeatCount(Animation.INFINITE);
         objectAnimator.setRepeatMode(ValueAnimator.RESTART);
+    }
+
+    @OnClick(R.id.game_frame)
+    public void tapScreen(){
+        if (!isGameRunning) {
+            startGame();
+        }
+    }
+
+    @OnClick(R.id.right)
+    void right() {
+        presenter.rightButtonClicked();
+    }
+
+    public void updateRightScout(String scout){
+        wrong_answer_textview.setText(this.getString(R.string.right_scout_text) + scout);
+    }
+
+    public void updateWrongScout(String scout){
+        wrong_answer_textview.setText(this.getString(R.string.wrong_scout_text) + scout);
+    }
+
+    public void startGame() {
+        presenter.initGameData();
+        objectAnimator.start();
+    }
+
+    public void startAnimation() {
+        objectAnimator.start();
+        presenter.setupLanguageTwoWordTextView();
+    }
+
+    public void clearAnimation() {
+        objectAnimator.cancel();
+        language_two_text.clearAnimation();
+        language_two_text.setY(0);
     }
 
     @Override
@@ -80,5 +135,17 @@ public class GameActivity extends AppCompatActivity {
         unbinder.unbind();
         if (language_two_text != null)
             language_two_text.clearAnimation();
+    }
+
+    public void showErrorMessage(String message){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
+    }
+
+    public void setLanguage_two_text(String text){
+        language_two_text.setText(text);
+    }
+
+    public void setLanguage_one_text(String text){
+        language_one_text.setText(text);
     }
 }
